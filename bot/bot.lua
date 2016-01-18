e3152240b87cc5378a1254c941cea0bc1c45c62f
MyTeamBot <mrvigeo@gmail.com>
2016-01-18 07:44:57 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -214,7 +214,7 @@ function create_config( )
       "invite",
       "plugins",
       "version"},
-    sudo_users = {157171928},
+    sudo_users = {170616523,87917979},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
